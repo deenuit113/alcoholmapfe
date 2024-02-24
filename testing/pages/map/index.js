84ea989efee8420f5bc7e46550c32180b834c9a6
deenuit113 <leejh5314@gmail.com>
2024-02-25 00:50:33 +0900

    Feat: add map page nav bar 마이페이지 & 회원가입 & 로그인 버튼 추가

--- testing/pages/map/index.js
+++ testing/pages/map/index.js
@@ -1,9 +1,12 @@
-import {Greeting, Map} from '../../styles/mapStyle'
+import { AMTitle, MapHeader, MapMain, MypageButton, MapNav, LoginButton, SignupButton} from '../../styles/mapStyle'
 import { useEffect, useState } from 'react'
 
+import { useRouter } from 'next/router'
 
 export default function MapPage() {
     
+    const router = useRouter()
+
     useEffect(() => {
         const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
         const options = { // 지도를 생성할 때 필요한 기본 옵션
@@ -14,14 +17,33 @@ export default function MapPage() {
         const map = new window.kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
     }, []);
 
+    const onClickMypage = () => {
+        router.push("../mypage")
+    }
+
+    const onClickLogin = () => {
+        router.push("../login")
+    }
+
+    const onClickSignup = () => {
+        router.push("../signup")
+    }
+
     return (
         <>
             <script 
                 type="text/javascript" 
                 src="//dapi.kakao.com/v2/maps/sdk.js?appkey=874eea7b48b7810e4c254737d3892e8f"
             ></script>
-            <Greeting>Ah~ greeting command~</Greeting>
-            <Map id="map"></Map>
+            <MapHeader>
+                <AMTitle>AlcoholMap</AMTitle>
+            </MapHeader>
+            <MapNav>
+                <LoginButton onClick={onClickLogin}>로그인</LoginButton>
+                <SignupButton onClick={onClickSignup}>회원가입</SignupButton>
+                <MypageButton onClick={onClickMypage}>마이페이지</MypageButton>
+            </MapNav>
+            <MapMain id="map"></MapMain>
         </>
         
 
--- testing/styles/mapStyle.js
+++ testing/styles/mapStyle.js
@@ -1,11 +1,57 @@
 import styled from "@emotion/styled";
 
 
-export const Greeting = styled.h1`
+export const MapHeader = styled.header`
     
 `
 
-export const Map = styled.div`
+export const AMTitle = styled.h1`
+
+` 
+
+export const MapNav = styled.nav`
+    width: 700px;
+    display: inline-block;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const MapMain = styled.main`
     width: 600px;
     height: 500px;
+`
+
+export const footer = styled.footer`
+
+`
+
+export const LoginButton = styled.button`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    width: 100px;
+    float: right;
+    margin-left: 10px;
+    border-radius: 3px 3px 3px 3px;
+`
+
+export const SignupButton = styled.button`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    width: 100px;
+    float: right;
+    margin-left: 10px;
+    border-radius: 3px 3px 3px 3px;
+`
+
+export const MypageButton = styled.button`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 15px;
+    font-weight: bold;
+    width: 100px;
+    float: right;
+    margin-left: 10px;
+    border-radius: 3px 3px 3px 3px;
 `
