e87c1133b70cdf24f5734a4680aaeda9d3eb56e5
deenuit113 <leejh5314@gmail.com>
2024-03-17 18:40:34 +0900

    Feat: add login and signup button on main page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,22 @@
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
+  const onClickMoveToLogin = () => {
+        router.push("../login")
+  }
+  const onClickMoveToSignup = () => {
+    router.push("../signup")
+}
   return (
     <>
       <main className={styles.main}>
           <p>
-            안녕하세요, 알콜맵입니다.
+            안녕하세요, 그린보틀입니다.
           </p>
+          <button onClick={onClickMoveToLogin}>로그인</button>
+          <button onClick={onClickMoveToSignup}>회원가입</button>
       </main>
     </>
   )
