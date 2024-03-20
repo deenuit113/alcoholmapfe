a0b7903a1f516cdf34b1c5d2ec8f313cd9e8b9ed
deenuit113 <leejh5314@gmail.com>
2024-03-20 15:06:56 +0900

    Chore: modify next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   async rewrites() {
     const BASE_URL = "https://greenbottle.site";
     return [
@@ -28,4 +28,4 @@ module.exports = {
       }
     ];
   },
-};
+};*/
