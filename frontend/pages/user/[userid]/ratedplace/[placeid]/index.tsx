bb402177f2bd099d58a0a1ba06476463b09b6dba
deenuit113 <leejh5314@gmail.com>
2024-04-10 19:34:49 +0900

    Refactor: refactor slider

---
+++ frontend/pages/user/[userid]/ratedplace/[placeid]/index.tsx
@@ -0,0 +1,7 @@
+import PlaceDetail from "../../../../../src/components/commons/placedetail/PlaceDetail.container";
+
+export default function RatedPlaceDetail(): JSX.Element {
+    return (
+        <PlaceDetail/>
+    );
+}
