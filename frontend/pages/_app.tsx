d2eca5a19f97d8b63c530d6abe0eb147f309fa5b
deenuit113 <leejh5314@gmail.com>
2024-04-15 22:23:42 +0900

    Feat: add tab title and icon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+    return (
+        <>
+            <Head>
+                <title>GreenBottle</title>
+                <link rel="icon" href="/sojuicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+        
+    );
 }
